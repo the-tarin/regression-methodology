86755867b08b40f08311f2d0c40b28faa9b5ef7e
Tarin Eccleston <tarin.eccleston@gmail.com>
2024-08-12 19:30:45 +1200

    finish assignment 1, create framework for assignment 2

--- cycling-in-the-rain/libs/weighted_distance_rainfall_average.R
+++
@@ -1,6 +0,0 @@
-weighted_distance_rainfall_average <- function(a_distance, b_distance, a_rainfall, b_rainfall) {
-  numer <- a_rainfall/a_distance + b_rainfall/b_distance
-  demon <- 1/a_distance + 1/b_distance
-  weighted_average <- numer/denom
-  return weighted_average
-}
